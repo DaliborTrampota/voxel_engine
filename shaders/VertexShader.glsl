a5bc6f090916d16d5dc6ffca86bbdf16964e529c
Dalibor Trampota <dalibor.trampota@gmail.com>
2025-02-19 09:12:54 +0100

    stb, perlin and shaders

---
+++ shaders/VertexShader.glsl
@@ -0,0 +1,28 @@
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aUV;
+layout (location = 3) in float aData;
+
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+
+out vec3 pos;
+out vec3 normal;
+out vec2 uv;
+
+flat out uint texID;
+out float ao;
+
+void main()
+{
+    texID = uint(aData) & 1023u;
+    ao = 1.0 - ((uint(aData) >> 10) & 3u) / 5.0;
+
+    uv = aUV;
+    pos = aPos;
+    normal = aNormal;
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+}
