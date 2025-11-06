7258f29130f2de4a96dda62002403bede561ee84
Dalibor Trampota <dalibor.trampota@autodesk.com>
2025-11-06 14:59:01 +0100

    test win guard

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -8,10 +8,12 @@
 #include <iostream>
 #include <vector>
 
+#ifdef _WIN32
 extern "C" {
 __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;  // for nvidia
 __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;    // for amd
 }
+#endif
 
 std::vector<GLuint64> m_textureHandles;
 std::vector<GLuint> m_textures;
