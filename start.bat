d8cc77dcf3a406d366d2d1253de3a741a1634c4b
Dalibor Trampota <dalibor.trampota@autodesk.com>
2025-11-06 13:14:36 +0100

    ninja for compile_commands.json

--- start.bat
+++ start.bat
@@ -4,6 +4,12 @@ if not exist build (
     mkdir build
 )
 
+if "%1" == "-cc" (
+    @REM cmake --preset ninja-clangd
+    move /y .\build-ninja\compile_commands.json .\build\compile_commands.json
+    exit
+)
+
 if "%1" == "-vs" (
     start build/voxel_engine.sln
     exit
