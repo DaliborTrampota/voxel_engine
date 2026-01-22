4794a21c7d7b0a1dbb0fc90fa258b45e55fea341
Dalibor Trampota <dalibor.trampota@autodesk.com>
2026-01-22 11:44:16 +0100

    render pass base resolution 2

--- engine/src/render/concrete/TransparentPass.cpp
+++ engine/src/render/concrete/TransparentPass.cpp
@@ -9,12 +9,9 @@ using namespace engine;
 void TransparentPass::beforeRender(Engine& engine, uint8_t pass) {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    // Two-pass rendering for correct transparency on convex objects:
     if (pass == 0) {
-        // Pass 1: Render back faces first (cull front)
         glCullFace(GL_FRONT);
     } else {
-        // Pass 2: Render front faces on top (cull back)
         glCullFace(GL_BACK);
     }
 }
