a5f53ec0317758cc9a0f45a6625e6ce26bf64c23
Dalibor Trampota <dalibor.trampota@autodesk.com>
2026-01-29 14:49:10 +0100

    cascade count sun

--- engine/src/scene/Sun.cpp
+++ engine/src/scene/Sun.cpp
@@ -12,6 +12,7 @@
 #include "../Globals.h"
 #include "data/TextureManager.h"
 #include "physics/AABB.h"
+#include "render/Material.h"
 #include "scene/Camera.h"
 
 
@@ -26,6 +27,7 @@ Sun::Sun(glm::ivec2 resolution, const Camera* target, const glm::vec3& direction
           "SunDepthShader"
       ),
       m_direction(glm::normalize(direction)) {
+    Material::setConstant("CascadeCount", static_cast<int>(m_cascadeSplits.size()));
     setTarget(target);
 
     m_lightSpaceUBO = gl::UBO(
